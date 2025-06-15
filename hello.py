a43cf13de4c222dd4b5b00c6bcac581b57f65c5b
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2025-06-15 14:30:38 +0200

    Add artifact

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from prefect import flow
+from prefect.artifacts import create_markdown_artifact
 
 @flow
 def hello_world():
+    create_markdown_artifact("Hello from prefect-workflow!")
     print("Hello from prefect-workflow!")
 
 
